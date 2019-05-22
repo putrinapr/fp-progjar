b7c65f85eb2704ec61ca212e56e0ea7cc47e6dbf
FalahFilano <falahnurli10@gmail.com>
2019-05-22 15:26:57 +0700

    tambah tugas 5

---
+++ Tugas 5/runserver_thread.sh
@@ -0,0 +1,10 @@
+#jalankan 5 async_server
+
+python thread_server.py 9001 &
+python thread_server.py 9002 &
+python thread_server.py 9003 &
+python thread_server.py 9004 &
+python thread_server.py 9005 &
+
+
+
